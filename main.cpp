89e69706f22566f510cb5c65248f873d13501893
AlfredoFrontera <alfredo.frontera2@upr.edu>
2025-11-07 05:30:24 +0000

    implementadas funciones de grafo

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include "Graph.hpp"
+#include <iostream>
 
 using namespace std;
 
 int main() {
-  Graph G(10);
-  G.addEdge(0,9);
-  G.addEdge(4, 2);
+    Graph G(10);
+    G.addEdge(0, 9);
+    G.addEdge(4, 2);
+    G.addEdge(3, 2);
+    G.addEdge(1, 2);
 
-  G.print();
+    cout << "Matriz:" << endl;
+    G.print();
+    
+    cout << "\nTotal de aristas: " << G.numEdges() << endl;
+    
+    cout << "\nIn-degrees:" << endl;
+    cout << "Nodo 2: " << G.inDegree(2) << endl;
+    cout << "Nodo 9: " << G.inDegree(9) << endl;
+    
+    cout << "\n¿Nodo 2 es influencer? " << (G.isInfluencer(2) ? "Si" : "No") << endl;
+    
+    return 0;
 }
